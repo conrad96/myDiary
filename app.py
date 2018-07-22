681c17f19d119378d84e621c8eed800fc72bdc33
conrad96 <conrad.mugisha@gmail.com>
2018-07-22 23:34:53 +0300

    Extra UI modification

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,redirect,url_for
+import os
 app=Flask(__name__)
 
 @app.route('/')
@@ -14,7 +14,17 @@ def register():
 def login():
 	return render_template('register.html')	
 
- if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+@app.route('/user')
+def user():
+	return render_template('user.html')	
+
+@app.route('/entries')
+def entries():
+	return render_template('entries.html')	
+@app.route('/logout')
+def logout():
+	return redirect(url_for('index'))
+if __name__ == '__main__':
+	app.debug = True
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
