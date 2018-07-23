f6c1058a858c27d73a8c55f2cd2f711e1f6c4eef
conrad96 <conrad.mugisha@gmail.com>
2018-07-23 03:04:03 +0300

    More user template edits

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,8 @@
+/*display entry form*/
 function entry(){
 	document.getElementById("new-entry").style.display='block';
+}
+/*display Posted  posts*/
+function post(){
+	document.getElementById("")
 }
