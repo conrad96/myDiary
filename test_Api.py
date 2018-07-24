65ec4e3a67472d626c4a49c90d0a2909f6c692bd
conrad96 <conrad.mugisha@gmail.com>
2018-07-24 15:28:06 +0300

    bug fixes

--- test_Api.py
+++ test_Api.py
@@ -7,18 +7,11 @@ class TestApi(unittest.TestCase):
 	def setUp(self):
 		self.app=app.app.test_client()
 		self.entry_id=1
-
+		'''test all entries response'''
 	def test_allEntries(self):
 		response=self.app.get('/api/v1/entries/')
 		self.assertEqual(response.status_code,200)
 		self.assertEqual(response.content_type,'application/json')
-
-	
-
-	# def test_allEntries(self):
-	# 	response=self.app.get('/api/v1/entries/')
-	# 	self.assertEqual(response.status_code,200)
-	# 	self.assertEqual(response.content_type,'application/json')		
 
 if __name__=='__main__':
 	unittest.main()		
