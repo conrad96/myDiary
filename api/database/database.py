0766282fd9b88352de12d1598cc59443112a19d5
conrad96 <conrad.mugisha@gmail.com>
2018-07-26 16:46:32 +0300

    database login functionalty

--- api/database/database.py
+++ api/database/database.py
@@ -1,16 +1,35 @@
 import psycopg2
-from pprint import pprint
-import os,sys
+import psycopg2.extras as ex
+import os,sys,json
+import jsonify
 
-class DB_Connection():
+class Database():
 	def __init__(self):
 		self.connection=psycopg2.connect("dbname='myDiary' user='postgres' password='715' port='5432' ")
 		self.cursor=self.connection.cursor()
 
+	'''function to add/register user account '''
 	def addUser(self,fullnames,username,password):
-		self.cursor.execute("INSERT INTO users(user_id,fullnames,username,password) VALUES( DEFAULT,%s,%s,%s)",(fullnames,username,password))
-		self.connection.commit()
-		self.cursor.close()
-		
-db=DB_Connection()
-db.addUser("mark cuban","cuban12","qwerty")	
+		try:
+			self.cursor.execute("INSERT INTO users(user_id,fullnames,username,password) VALUES( DEFAULT,%s,%s,%s)",(fullnames,username,password))
+			self.connection.commit()
+			self.cursor.close()
+		except psycopg2.Error as e:
+			print(e.pgerror)
+	'''function to Login user '''			
+	def loginUser(self,username,password):
+		try:
+			self.cursor=self.connection.cursor(cursor_factory=ex.DictCursor)
+			self.cursor.execute("SELECT * FROM users WHERE username='"+username+"' AND password='"+password+"' ")
+			rows=self.cursor.fetchall()
+			if  rows!=[] :				
+				return rows
+			else:
+			 	return False
+		except psycopg2.Error as e:
+			print(e.pgerror)			
+
+db=Database()
+#db.addUser("mark cuban","cuban12","qwerty")
+output=db.loginUser('conrad96','qwerty')	
+print(output)
