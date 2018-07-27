747f8ea153d38e356db342ec4aabb75dbef31f39
conrad96 <conrad.mugisha@gmail.com>
2018-07-27 10:20:29 +0300

    Database API endpoints methods

--- app.py
+++ app.py
@@ -1,32 +1,46 @@
 
 from flask import Flask,render_template,redirect,url_for,request,jsonify,abort,make_response
 from database import Database
+from functools import wraps
 import os,json,jwt
 import datetime
+
 app=Flask(__name__)
 db=Database()
-app.config['SECRET_KEY']='mysecret123'
-entries=[
-{"entry_id":1,"username":"Bill12","title":"My Day 1","body":"Dear Diary Today was so exhausting 1","date":"7-23-2018"},
-{"entry_id":3,"username":"Ariho12","title":"My Day 3","body":"Dear Diary Today was so exhausting 3","date":"7-24-2018"},
-{"entry_id":4,"username":"mugisha12","title":"My Day 4","body":"Dear Diary Today was so exhausting 4","date":"7-25-2018"},
-{"entry_id":2,"username":"Henry12","title":"My Day 2","body":"Dear Diary Today was so exhausting 2","date":"7-26-2018"}]
+app.config['SECRET_KEY']='conrad12'
 
-'''get all users '''
+''' token verification method (time,secretkey) '''
+def token_verify(f):
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		token=request.args.get('token')
+		if not token:
+			return jsonify({"response":"Token String is Null "}),401
+		try:
+			data=jwt.decode(token,app.config['SECRET_KEY'])
+		except:
+			return jsonify({"response":"Token is INVALID OR EXPIRED"}),401
+
+		return f(*args,**kwargs)	
+	return decorated_function	
+
+'''method to fetch  all users from database '''
 @app.route("/api/v1/users/",methods=["GET"])
 def api_users():
 	response=db.getUsers()
 	return jsonify({"users":response})
 
-'''add new user to database return response on activity'''
-@app.route("/api/v1/users/",methods=["POST"])
+'''add new user to database return appopriate response'''
+@app.route("/api/v1/auth/signup",methods=["POST"])
 def api_Adduser():
 	fullnames=request.json['fullnames']
 	username=request.json['username']
 	password=request.json['password']
 	check=db.addUser(fullnames,username,password)
-	return jsonify({"response":check})
-'''login user'''
+	if not check:
+		return jsonify({"response":"Registration Failed"})
+
+'''login user and return Valid Token '''
 @app.route("/api/v1/auth/login/",methods=['POST'])
 def api_login():
 	auth=request.authorization
@@ -34,42 +48,62 @@ def api_login():
 	password=request.json['password']
 	verify=db.loginUser(username,password)
 	if verify:
-		token=jwt.encode({"user":username,'exp':datetime.datetime.utcnow()+datetime.timedelta(seconds=10)},app.config['SECRET_KEY'])
+		token=jwt.encode({"user":username,'exp':datetime.datetime.utcnow()+datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
 		return jsonify({"token":token.decode('UTF-8')})
 	else:
 		return jsonify({"response":"Incorrect password or Username"})	
-	# return make_response("Sorry Couldn't Verify",401,{'www-Authenticate':'Basic-Realm="Login Required"'})	
-'''get all entries'''
-@app.route("/api/v1/entries/",methods=["GET"])
-def api_entries():
-	return jsonify({"All_Entries":entries})
+	return make_response("Sorry Couldn't Verify",401,{'www-Authenticate':'Basic-Realm="Login Required"'})	
 
-'''search entry by entry_id'''
-@app.route('/api/v1/entries/<int:id>/',methods=["GET"])
-def api_searchEntry(id):
-	entry= [entry for entry in entries if entry["entry_id"]==id ]
-	if entry !=[]:
-		return jsonify({"Found_Entry":entry[0]})
-	else:
-		abort(404, {'message':'Resource Not Found'})
+''' Redirect to Authenticated users with Valid Token '''
+@app.route('/api/v1/user/')
+@token_verify
+def users_page():
+	return jsonify({"response":"Welcome Authenticated User"})	
 
-'''Add an Entry'''			
+'''User adds  an new Entry '''			
 @app.route('/api/v1/entries/',methods=["POST"])
 def api_addEntry():
-	add_entry=dict(entry_id=request.json['entry_id'],title=request.json['title'],body=request.json['body'],date=request.json['date'])
-	entries.append(add_entry)
-	return jsonify({"response":"Entry Added"})
+	user_id=request.json['user_id']
+	title=request.json['title']
+	body=request.json['body']
+	local = datetime.datetime.now() #insert local machines date
+	current_date = local.strftime("%Y-%m-%d")
+	entry=db.addEntry(user_id,title,body,current_date)#pass post request values to db method for adding Entry
+	if not entry:
+		return jsonify({"response":"Entry Not Added"})
+	return jsonify({"response":"Entry Added"})	
 
-'''modify an entry by searching the json object where it exists and new values assigned to the keys'''
-@app.route('/api/v1/entries/<int:id>',methods=["PUT"])
-def modifyEntry(id):
-	record= [entry for entry in entries if entry["entry_id"]==id ]
-	if record !=[]:
-		values=request.get_json()
-		edited=dict(entry_id=values['entry_id'],title=values['title'],date=values['date'],body=values['body'],username=values['username'])
-		return jsonify({"Edited":record})	
-	else:
-		abort(404, {'message':'Resource Not Found'})	
+'''Fecth all entries of a user '''
+@app.route('/api/v1/entries/',methods=["GET"])
+def api_fetchEntries():
+	rows=db.getEntries()
+	return jsonify({"Entries":rows})
+
+'''Fetch entries of single user'''
+@app.route('/api/v1/entries/<int:id>/',methods=["GET"])
+def api_getuserEntry(id):
+	user_id=str(id)
+	fetch=db.getuserEntries(user_id)
+	if not fetch:
+		return jsonify({"response":"Empty no Entries Found"})
+	return jsonify({"User Entrys":fetch })	
+
+'''modify an entry of '''
+@app.route('/api/v1/entries/<int:entry_id>',methods=["PUT"])
+def api_modifyEntry(entry_id):
+	entry_id=str(entry_id)
+	title=request.json['title']
+	body=request.json['body']
+	get_date=db.getDate(entry_id)
+	local = datetime.datetime.now()
+	current_date = local.strftime("%Y-%m-%d")
+	if get_date != current_date: 	#compare  postDate and Localdate 
+		return jsonify({"response":"Sorry You can only Modify Today's Entries "})
+	else:	
+		fetch=db.modifyEntry(entry_id,title,body,current_date)
+		if not fetch:
+			return jsonify({"response":"Entry Not Modified"})
+	return jsonify({"response": "Entry Modified successfully" })
 
 '''custom ErrorHandler for resource not found 404'''
 @app.errorhandler(404)
--- database.py
+++ database.py
@@ -2,13 +2,13 @@ import psycopg2
 import psycopg2.extras as ex
 import os,sys,json
 import jsonify
-
+import datetime
 class Database:
 	def __init__(self):
 		self.connection=psycopg2.connect("dbname='myDiary' user='postgres' password='715' port='5432' ")
 		self.cursor=self.connection.cursor()
 
-	'''function to add/register user account '''
+	'''Method to add/register user account '''
 	def addUser(self,fullnames,username,password):
 		try:
 			self.cursor.execute("INSERT INTO users(user_id,fullnames,username,password) VALUES( DEFAULT,%s,%s,%s)",(fullnames,username,password))
@@ -17,7 +17,7 @@ class Database:
 		except psycopg2.Error as e:
 			print(e.pgerror)
 			return False
-	'''function to Login user '''			
+	'''Method to Login user by validating the username and password '''			
 	def loginUser(self,username,password):
 		try:
 			self.cursor=self.connection.cursor(cursor_factory=ex.DictCursor)
@@ -30,7 +30,7 @@ class Database:
 		except psycopg2.Error as e:
 			print(e.pgerror)
 
-	'''function to retrieve all users '''			
+	'''Method to retrieve all users '''			
 	def getUsers(self):
 		try:
 			self.cursor=self.connection.cursor(cursor_factory=ex.DictCursor)
@@ -41,5 +41,84 @@ class Database:
 			else:
 			 	return False
 		except psycopg2.Error as e:
-			print(e.pgerror)		
+			print(e.pgerror)
 
+	'''Method to add a single Entry into the DB'''		
+	def addEntry(self,user_id,title,body,date_posted):			
+		try:
+			self.cursor=self.connection.cursor(cursor_factory=ex.DictCursor)
+			self.cursor.execute("INSERT INTO entries (user_id,title,body,date_posted,entry_id) VALUES( %s,%s,%s,%s,DEFAULT)",(user_id,title,body,date_posted))
+			self.connection.commit()
+			return True
+		except psycopg2.Error as e:
+			print(e.pgerror)
+			return False
+
+	'''Method to fetch all Entries in the Database'''		
+	def getEntries(self):
+		try:
+			self.cursor=self.connection.cursor(cursor_factory=ex.DictCursor)
+			self.cursor.execute("SELECT * FROM entries")
+			rows=self.cursor.fetchall()
+			if  rows!=[] :				
+				return rows
+			else:
+			 	return False
+		except psycopg2.Error as e:
+			return e.pgerror	
+
+	'''Method to fetch all a users Entries basing on his user_id'''		
+	def getuserEntries(self,user_id):
+		try:
+			self.cursor=self.connection.cursor(cursor_factory=ex.DictCursor)
+			self.cursor.execute("SELECT * FROM entries WHERE user_id='"+user_id+"' ")
+			rows=self.cursor.fetchall()
+			if  rows!=[] :				
+				return rows
+			else:
+			 	return False
+		except psycopg2.Error as e:
+			return e.pgerror				
+
+	def modifyEntry(self,entry_id,title,body,date):
+		try:
+			self.cursor=self.connection.cursor(cursor_factory=ex.DictCursor)
+			self.cursor.execute("UPDATE entries SET  title=%s , body=%s , date_posted=%s  WHERE entry_id='"+entry_id+"' ",(title,body,date))
+			updated_rows = self.cursor.rowcount
+			self.connection.commit()
+			return True
+		except psycopg2.Error as e:
+			print(e.pgerror)
+			return False
+				
+	def getDate(self,entry_id):
+		try:
+			entry_id=str(entry_id)
+			self.cursor=self.connection.cursor(cursor_factory=ex.DictCursor)
+			self.cursor.execute("SELECT row_to_json(t) FROM ( select date_posted from entries where entry_id='"+entry_id+"' ) t ")
+			rows=self.cursor.fetchall()
+			if  rows!=[] :
+				datePost=""
+				for y in rows:
+					for z in y:
+						datePost=z['date_posted']
+				return datePost
+			else:
+			 	return False
+			self.connection.commit()
+			return True
+		except psycopg2.Error as e:
+			print(e.pgerror)
+			return False		
+db=Database()
+# x=db.getDate(3)
+# print(type(x))
+
+#x=db.modifyEntry("3","Love","I love this girl","2018-07-02")
+#print(x)			
+now = datetime.datetime.now()
+# yr="%d" %now.year
+# mnth="%d"%now.month
+# day="%d"%now.day
+date= now.strftime("%Y-%m-%d")
+print(date)
